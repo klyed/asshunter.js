a2d2abc44f9186837a4204172e1f22c479305cd9
klyed <kylejrturnbull@gmail.com>
2021-05-04 18:37:19 -0500

    Hive Update

--- asshunter.js
+++ asshunter.js
@@ -1,98 +1,92 @@
 //--------------------------------------------------------------------------
 //----- AssHat Hunter v0.0.1 || Open Source Anti-Asshat Node.js Script -----
 //----- Developed by @KLYE || Free to Use for All! || Free to Modify -------
-//----- Rekuirements to run: Node.js + steem.js + node-datetime  -----------
-//----- npm install steem --save  ||  npm install node-datetime --save -----
+//----- Rekuirements to run: Node.js + @hiveio/hive-js + node-datetime  -----------
+//----- npm install @hiveio/hive-js --save  ||  npm install node-datetime --save -----
+//----- npm install dotenv --save
 //--------------------------------------------------------------------------
 //----- PLEASE DO NOT USE THIS CODE BELOW MALICIOUSLY / FOR EVIL DEEDS -----
 //--------------------------------------------------------------------------
 
+// No need to modify these variables
+var hivejs = require("@hiveio/hive-js");
+var dateTime = require('node-datetime');
+const dotenv = require('dotenv');
+dotenv.config();
+var dt = dateTime.create();
 
-//----- CONFIG + Get Rekuirements 
+var hero = process.env.HERO;
 
-// ***IMPORTANT*** Your Account Name Below (No @)
-var hero = 'youraccount';
-
-// ***IMPORTANT*** Your Posting Private Key Below
-var herowifkey = '5P0St1nGpR1vK3ys7rz9zJzLGjgHn4aWX79MC6jh3y3W3DJk';
+var herowifkey = process.env.HERO_WIF_KEY;
 
 // ***IMPORTANT*** Enter Asshat's Account Below (No @)
-var asshat = "asshatsaccount";
+var asshat = "nobutsd";
 
-// No need to modify these variables
-var steem = require('steem');
-var dateTime = require('node-datetime');
-var dt = dateTime.create();
 var totalblocks = 0;
 var totalvote = 0;
 var totalcomment = 0;
+var totalops = 0;
+var apiindex = 1;
 
-//----- Script Started + Show Time
-     var starttime = dt.format('Y-m-d H:M:S');
-console.log("===================================================================");
-console.log("-------------------------------------------------------------------");
-console.log("AssHat Hunter v0.0.1 ONLINE - By @KLYE - Listening to STEEM Network");
-console.log("-------------------------------------------------------------------");
-console.log("===================================================================");
-console.log("---------------- Start Time: " + starttime + " ------------------");
-console.log("-------------------------------------------------------------------");
-console.log(" ");
+const apinodes = ["hived.privex.io", "api.hivekings.com", "api.deathwing.me", "api.hive.blog", "api.openhive.network", "hive.loelandp.nl", "hive-api.arcange.eu", "rpc.ausbit.dev", "anyx.io"];
 
-//----- Grab Current STEEM Block
-steem.api.streamBlockNumber(function(err1, newestblock) {
+hivejs.api.setOptions({ url: "https://api.hivekings.com" });
+
+async function changenode() {
+  if (apiindex < apinodes.length){
+    apiindex++;
+  } else if (apiindex == apinodes.length) {
+    apiindex = 0;
+  }
+  console.log(`CHAIN: Changed API Node to ${apinodes[apiindex]}`);
+  await hivejs.api.setOptions({ url: `https://${apinodes[apiindex]}` });
+}
+
+  var starttime = dt.format('Y-m-d H:M:S');
+
+  console.log("\nShitList v0.0.1 Anti Phish - By @KLYE");
+  console.log("Start Time: " + starttime + "\n");
+
+
+  var process_comment = function(op) {
+    if (op["author"] == asshat) {
+      totalcomment++
+      console.log("-------------------------------------------------------------");
+      console.log("----- ASSHAT COMMENT DETECTED - Deploying Counter Comment! -----");
+      console.log("-------------------------------------------------------------");
+      hivejs.broadcast.comment(herowifkey, op["author"], op["parent_permlink"], hero, op["author"], "WARNING", "<h1>DO NOT CLICK ON THE LINK ABOVE!</h1>\nThe Account above was recently hacked in a phishing attack and is spamming for more victims.", {}, function(err, result) {
+        console.log(err, result);
+      });
+    }
+  };
+
+  function parseBlock(blockNum) {
+    hivejs.api.getBlock(blockNum, function(err, block) {
+      if(err){
+        return console.log(`Ooops. Parsed too fast!`);
+      }
+      if (err !== null) {
+        console.log(err);
+      };
+      if(block){
+        block.transactions.forEach((transaction) => {
+          transaction.operations.forEach((op) => {
+            totalops++;
+            var opType = op[0];
+            var opgrab = op[1];
+            if (opType == "comment") {
+              process_comment(opgrab);
+            }
+          });
+        });
+      }
+    });
+  }
+
+  hivejs.api.streamBlockNumber(async function(err1, newestblock) {
+    if(err1) changenode();
+    if(newestblock) await parseBlock(newestblock);
     totalblocks++
-    console.log("-------------------------------------------------------------------");
-    console.log("Scanning STEEM Block #" + newestblock + " for Asshat Account @" + asshat);
-    console.log("-------------------------------------------------------------------");
-    console.log("Blocks Monitored this Session Against Asshats = " + totalblocks);
-    console.log("Upvotes This Session Against Asshats = " + totalvote);
-    console.log("Comments Flagged This Session Against Asshats = " + totalcomment);
-    console.log(" ");
-});
-
-//NOTE: Sometimes the Script Fails to Hook Into STEEM. Try Running Script Again if it Fails!
-
-
-//----- See if Vote is our Target Asshat ----
- var process_vote = function(op) {
-      if (op["voter"] == asshat) {
-        if(op["weight"] < 0 ){
-          totalvote++
-          console.log("-------------------------------------------------------------");
-          console.log("----- ASSHAT FLAG DETECTED - Deploying Counter Upvote! ------");
-          console.log("-------------------------------------------------------------");
-
-          steem.broadcast.vote(
-            herowifkey,
-            hero,
-            op["author"],
-            op["permlink"],
-            10000,
-              function(uperr, result) {
-              console.log(uperr, result);
-              }
-            );
-        }
-      }
-  };
- 
-//----- See if Comment is our Target Asshat ---- 
- var process_comment = function(op) {
-      if (op["author"] == asshat) {
-        totalcomment++
-        console.log("-------------------------------------------------------------");
-        console.log("----- ASSHAT COMMENT DETECTED - Deploying Counter Flag! -----");
-        console.log("-------------------------------------------------------------");
-
-        steem.broadcast.vote(
-            herowifkey,
-            hero,
-            op["author"],
-            op["permlink"],
-            -10000,
-              function(downerr, result) {
-              console.log(downerr, result);
-              }
-        );       
-      }
-  };
+    process.stdout.write(`SHITLIST: @${asshat} - #${newestblock} - Blocks Monitored: ${totalblocks} - Ops Scanned: ${totalops} - Phishes Fried: ${totalcomment}`);
+    process.stdout.cursorTo(0);
+  });
